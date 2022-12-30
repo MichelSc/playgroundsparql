bc2b54166de29104ef3f65905e171e101ce451cf
michel <michelschaffers@gmail.com>
2022-12-30 14:48:29 +0100

    Fine tuning

--- scripts/importrdf.sh
+++ scripts/importrdf.sh
@@ -1,4 +1,6 @@
 #param1 file to be imported, relative to the workspace
 echo importrdf $1
 source .env
-echo $ROOT_PWD | sudo -S docker run  -t -v `pwd`:/wks -v `pwd`/home:/opt/graphdb/home --entrypoint "importrdf" $GRAPHDB_IMAGE load -Dgraphdb.home=/opt/graphdb/home -f -c /wks/conf/repo-config.ttl -m parallel /wks/$1
+# first -v is for passing the file to be imported
+# second -v is for passing the home folder, where the target repository resides
+echo $ROOT_PWD | sudo -S docker run -t -t -v `pwd`:/wks -v `pwd`/home:/opt/graphdb/home --entrypoint "importrdf" $GRAPHDB_IMAGE load -Dgraphdb.home=/opt/graphdb/home -f -i $GRAPHDB_REPOSITORY -m parallel /wks/$1
