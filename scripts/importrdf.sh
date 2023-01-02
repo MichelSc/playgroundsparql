4fb3e3244e109b1a3f9bd857ee51e0a545ff3ed7
Michel Schaffers <michel.schaffers@flandersmake.be>
2023-01-02 08:40:18 +0100

    typo

--- scripts/importrdf.sh
+++ scripts/importrdf.sh
@@ -3,4 +3,4 @@ echo importrdf $1
 source .env
 # first -v is for passing the file to be imported
 # second -v is for passing the home folder, where the target repository resides
-echo $ROOT_PWD | sudo -S docker run -t -t -v `pwd`:/wks -v `pwd`/home:/opt/graphdb/home --entrypoint "importrdf" $GRAPHDB_IMAGE load -Dgraphdb.home=/opt/graphdb/home -f -i $GRAPHDB_REPOSITORY -m parallel /wks/$1
+echo $ROOT_PWD | sudo -S docker run -t -v `pwd`:/wks -v `pwd`/home:/opt/graphdb/home --entrypoint "importrdf" $GRAPHDB_IMAGE load -Dgraphdb.home=/opt/graphdb/home -f -i $GRAPHDB_REPOSITORY -m parallel /wks/$1
