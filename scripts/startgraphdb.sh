0286e5772d5f7c9fe7e5a98bbcf863ca0862888c
Michel Schaffers <michelschaffers@gmail.com>
2024-02-20 08:47:52 +0100

    Better script

--- scripts/startgraphdb.sh
+++ scripts/startgraphdb.sh
@@ -1,3 +1,3 @@
 source .env
-echo $ROOT_PWD | sudo -S docker run -p $GRAPHDB_HOST:$GRAPHDB_PORT:7200 -v `pwd`/home:/opt/graphdb/home -t --name $GRAPHDB_CONTAINER $GRAPHDB_IMAGE
+echo $ROOT_PWD | sudo -S docker run -p $GRAPHDB_HOST:$GRAPHDB_PORT:7200 -v `pwd`/$GRAPHDB_HOME:/opt/graphdb/home -t --name $GRAPHDB_CONTAINER $GRAPHDB_IMAGE
 
