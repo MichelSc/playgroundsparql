9113ffe7099fe43822ede07cdc6df9a538fc9dc5
Michel Schaffers <michelschaffers@gmail.com>
2024-03-28 09:33:31 +0100

    Maintain playground

--- scripts/importrdf.sh
+++ scripts/importrdf.sh
@@ -1,6 +1,7 @@
 #param1 file to be imported, relative to the workspace
 echo importrdf $1
 source .env
+source ~/.secrets
 # first -v is for passing the file to be imported
 # second -v is for passing the home folder, where the target repository resides
 echo $ROOT_PWD | sudo -S docker run -t -v `pwd`:/wks -v `pwd`/home:/opt/graphdb/home --entrypoint "importrdf" $GRAPHDB_IMAGE load -Dgraphdb.home=/opt/graphdb/home -f -i $GRAPHDB_REPOSITORY -m parallel /wks/$1
--- scripts/startgraphdb.sh
+++ scripts/startgraphdb.sh
@@ -1,3 +1,4 @@
 source .env
+source ~/.secrets
 echo $ROOT_PWD | sudo -S docker run -p $GRAPHDB_HOST:$GRAPHDB_PORT:7200 -v `pwd`/$GRAPHDB_HOME:/opt/graphdb/home -t --name $GRAPHDB_CONTAINER $GRAPHDB_IMAGE
 
